ab4320f392cea008bd4d763426c2eeff0816cb14
gilar setiawan <gilarsr08@gmail.com>
2022-12-01 03:11:40 +0700

    menambahkan button login dan backgroun image

--- README.old.md
+++ README.old.md
@@ -1,2 +1,12 @@
 # mini_projek_2_vilm
 themovieDb vilm
+Yang di tambahkan rabu 1 Desember
+    -button login
+    -background image
+yang di instal
+    -react bootstrap
+    -bootstrap
+    -react aicons
+    -axios
+    -yup
+    -formik
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,24 +1,33 @@
-import { Container, Form, Nav, Navbar, Button } from "react-bootstrap"
+import { Container, Form, Nav, Navbar } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import { BsFillCaretRightSquareFill } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
+
 
 
 const NavbarMovie = () => {
     return(
-        <Navbar>
+        <Navbar expand="lg">
             <Container>
-                <Navbar.Brand>Movie DB</Navbar.Brand>
-                <Nav>
-                    <Nav.Link>Populer</Nav.Link>
-                    <Nav.Link>series</Nav.Link>
-                    <Nav.Link>action</Nav.Link>
-                    <Form className="searchMovie">
-                    <Form.Group className="mb-3" controlId="formBasicsearch">
-                        <Form.Control type="search" placeholder="search......" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-                </Nav>
+                <Navbar.Brand className="NavBrand text-white" href="#home" style={{fontSize:"2vw", }}>
+                    <BsFillCaretRightSquareFill style={{color:"aqua", fontSize:"3vw", paddingRight:"5px", fontFamily: 'Zen Dots-Cursive'}}/>
+                    BIOSKOP CINEMA
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" style={{backgroundColor:"white"}} />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="d-flex me-auto">
+                            <Nav.Link href="#home" style={{color:"white", fontFamily: 'FredokaOne-cursive', fontSize:"20px"}}>Home</Nav.Link>
+                            <Nav.Link href="#link" style={{color:"white", fontFamily: 'FredokaOne-cursive', fontSize:"20px"}}>Series</Nav.Link>
+                            <Nav.Link href="#home" style={{color:"white", fontFamily: 'FredokaOne-cursive', fontSize:"20px"}}>Action</Nav.Link>
+                            <Nav.Link href="#home" style={{color:"white", fontFamily: 'FredokaOne-cursive', fontSize:"20px"}}>Populer</Nav.Link>
+                        </Nav>
+                </Navbar.Collapse>
+                <Form.Group className="input">
+                            <Form.Control type="text" placeholder =" serach...." style={{borderRadius: "20px"}} />
+                        </Form.Group>
+                        <BsSearch style={{position: "relative", right:" 160px"}}/>
+                        <Button variant="outline-primary" style={{borderRadius: "20px", position: "relative", right: "33px"}}>LOGIN</Button>{' '}
+                        <Button variant="outline-primary" style={{borderRadius:"20px"}}>SIGIN</Button>{' '}
             </Container>
         </Navbar>
     )
